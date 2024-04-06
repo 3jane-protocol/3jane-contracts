d2d6fc74938578b1abedbcb7fe151e501a9e523b
Your Name <jacob4seinfeld@gmail.com>
2024-04-06 12:34:21 +0800

    Add subfolder

--- contracts/utils/EthenaDepositHelper.sol
+++ contracts/utils/EthenaDepositHelper.sol
@@ -2,7 +2,7 @@
 pragma solidity =0.8.4;
 
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import {ISUSDE} from "../interfaces/IEthena.sol";
+import {ISUSDE} from "../interfaces/basis/IEthena.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
 import {IERC20Detailed} from "../interfaces/IERC20Detailed.sol";
